dda0fa83a6e868261465a3fd87946e05e25ee562
Pratik Randive <121440105+pratikrandive04@users.noreply.github.com>
2023-12-22 15:27:57 +0530

    Update and rename practice1.java to 1. Write a Java program to print 'Hello' on screen and your name on a separate line.java

--- 1. Write a Java program to print 'Hello' on screen and your name on a separate line..java
+++ 1. Write a Java program to print 'Hello' on screen and your name on a separate line..java
@@ -1,4 +1,4 @@
-//Write a Java program to print 'Hello' on screen and your name on a separate line.
+//1Write a Java program to print 'Hello' on screen and your name on a separate line.
 
 public class practice1{
     public static void main(String args[]){
